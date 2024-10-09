c0e4cf6b41312ea0efb9fcf1b9719763511720d4
sofffja <sofi.efron@gmail.com>
2024-10-08 21:13:06 -0300

    Create color variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        text: "var(--text)",
+        secondary: "var(--secondary)",
+        background: "var(--background)",
+        accent: "var(--accent)",
+      },
+    },
   },
   plugins: [],
-}
+};
